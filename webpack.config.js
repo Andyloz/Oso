e05728902dda5e369e8ff43b1e418e41d411794c
andyloz <lozandyloz@gmail.com>
2021-02-17 21:42:10 +0100

    Reestructuración completada

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
+    entry: "./src/index.tsx",
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".sass"]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "src", "index.html")
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)?$/,
+                use: "ts-loader"
+            },
+            {
+                test: /\.sass$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    }
 }
