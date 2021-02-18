9353b022a5d05d3bd6fa9a12559afaa009e20e99
andyloz <lozandyloz@gmail.com>
2021-02-18 11:35:50 +0100

    Mejoras en la implementación de los componentes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,32 @@
 import React from 'react'
+import LineTo from 'react-lineto'
 
-const App: React.FunctionComponent = () => <p>Hola mundo !!!</p>
-export default App
+export default class App extends React.Component<{}, {}> {
+    constructor(props: {}) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <img src="https://mms.businesswire.com/media/20200203005689/en/699026/4/FSD_LOGO_Stack.jpg"
+                     alt="aa"
+                     className="A"
+                     style={{
+                         position: "absolute",
+                         top: "0"
+                     }}
+                />
+                <img src="https://mms.businesswire.com/media/20200203005689/en/699026/4/FSD_LOGO_Stack.jpg"
+                     alt="aa"
+                     className="B"
+                     style={{
+                         position: "absolute",
+                         bottom: "0"
+                     }}
+                />
+                <LineTo from="A" to="B"/>
+            </div>
+        )
+    }
+}
