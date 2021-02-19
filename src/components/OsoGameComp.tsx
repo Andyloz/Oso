0d923a3b3068f57f0c54033a24bfc88fd3e62f02
andyloz <lozandyloz@gmail.com>
2021-02-19 12:14:08 +0100

    Añade primera implementación para método solitaire

--- src/components/OsoGameComp.tsx
+++ src/components/OsoGameComp.tsx
@@ -1,13 +1,32 @@
 import OsoGame from "../scripts/OsoGame";
 import React from "react";
 import OsoGridComp from "./OsoGridComp";
+import OsoMatch from "../scripts/OsoMatch";
 
 interface Props {
     game: OsoGame
 }
 
-export default class OsoGameComp extends React.Component<Props, Record<string, never>> {
+interface State {
+    matches: OsoMatch[]
+}
+
+export default class OsoGameComp extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = { matches: this.props.game.matches }
+    }
+
+    handleClick(_: React.MouseEvent<HTMLButtonElement>): void {
+        return undefined
+    }
+
     render(): React.ReactNode {
-        return <OsoGridComp table={this.props.game.table}/>
+        return (
+            <div>
+                <OsoGridComp table={this.props.game.table}/>
+                <button>Solitaire</button>
+            </div>
+        )
     }
 }
