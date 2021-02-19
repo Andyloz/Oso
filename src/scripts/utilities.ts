0c0573b789a818cbb78d17eef9294ca57c2d7f99
andyloz <lozandyloz@gmail.com>
2021-02-19 10:42:58 +0100

    Añade el método reverseMap y corrige errores en el módulo de utilidades

--- src/scripts/utilities.ts
+++ src/scripts/utilities.ts
@@ -1,9 +1,13 @@
 export const Random = {
-    getNumber(min: number, maxExclusive: number) {
+    getNumber(min: number, maxExclusive: number): number {
         return Math.random() * (maxExclusive - min) + min
     },
 
-    getInteger(min: number, maxExclusive: number) {
+    getInteger(min: number, maxExclusive: number): number {
         return Math.floor(this.getNumber(min, maxExclusive))
     }
+}
+
+export function reverseMap<T, O>(arg: T[], fn: (a: T) => O): O[] {
+    return arg.map((_, i, arr) => fn(arr[arr.length - i - 1]))
 }
