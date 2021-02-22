8241f1abbd31ceb5c6f9dc917b4047434869cfb7
andyloz <lozandyloz@gmail.com>
2021-02-22 18:07:32 +0100

    Nueva utilidad Input.getInteger

--- src/scripts/utilities.ts
+++ src/scripts/utilities.ts
@@ -14,3 +14,16 @@ export function reverseMap<T, O>(arg: T[], fn: (a: T, index: number, arr: T[]) =
         return fn(arr[i], i, arr)
     })
 }
+
+export const Input = {
+    getInteger(elem: HTMLInputElement): number | null {
+        const input = elem.value || ""
+        const parsed = parseInt(input)
+
+        if (!Number.isNaN(parsed)) {
+            return parsed
+        } else {
+            return null
+        }
+    }
+}
