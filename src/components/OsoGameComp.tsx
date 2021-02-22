086536318f6be31f6b50846e4563e04154b23a04
andyloz <lozandyloz@gmail.com>
2021-02-22 15:42:48 +0100

    Nueva estructura de OsoGameComp

--- src/components/OsoGameComp.tsx
+++ src/components/OsoGameComp.tsx
@@ -1,33 +1,61 @@
-import OsoGame from "../scripts/OsoGame";
 import React from "react";
+import OsoGame from "../scripts/OsoGame";
 import OsoGridComp from "./OsoGridComp";
-import OsoMatch from "../scripts/OsoMatch";
-
-interface Props {
-    game: OsoGame
-}
 
 interface State {
-    matches: OsoMatch[]
+    game: OsoGame | null
 }
 
-export default class OsoGameComp extends React.Component<Props, State> {
-    constructor(props: Props) {
+export default class OsoGameComp extends React.Component<Record<string, never>, State> {
+    constructor(props: Record<string, never> = {}) {
         super(props);
-        this.state = { matches: this.props.game.matches }
         this.handleSolitaire = this.handleSolitaire.bind(this)
+        this.handleRegenerate = this.handleRegenerate.bind(this)
+        this.state = { game: null }
     }
 
     handleSolitaire(e: React.MouseEvent<HTMLButtonElement>): void {
-        this.props.game.solitaire()
-        this.setState({ matches: this.props.game.matches })
+        console.log("hS")
+    }
+
+    handleRegenerate(): void {
+        console.log("hR")
     }
 
     render(): React.ReactNode {
+        const game = this.state.game
+        const mainClassName = "main"
+        let main
+
+        if (game) {
+            main = (
+                <div className={mainClassName}>
+                    <OsoGridComp table={game.table} matches={game.matches}/>
+                    <button onClick={this.handleSolitaire}>Solitaire</button>
+                    <button onClick={this.handleRegenerate}>Regenerar tablero</button>
+                </div>
+            )
+        } else {
+            main = (
+                <div className={mainClassName}>
+                    <h1>Juego <strong>OSO</strong></h1>
+                    <form>
+                        <div>
+                            <label htmlFor="cols">Columnas</label>
+                            <input type="number" id="cols"/>
+                        </div>
+                        <div>
+                            <label htmlFor="rows">Filas</label>
+                            <input type="number" id="rows"/>
+                        </div>
+                    </form>
+                    <button>Jugar</button>
+                </div>
+            )
+        }
         return (
             <div className="OsoGame">
-                <OsoGridComp table={this.props.game.table} matches={this.state.matches}/>
-                <button onClick={this.handleSolitaire}>Solitaire</button>
+                { main }
             </div>
         )
     }
